7f2ad3856756b89b27a9760938582f0702dac05a
RaphaelLu <ronilu66@hotmail.com>
2016-11-22 23:54:55 -0800

    initiated SQLAlchemy db

---
+++ database.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///calls.db'
+db = SQLAlchemy(app)
+
+
+class Call(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    phone = db.Column(db.String(80))
+    delay = db.Column(db.Integer)
+    number = db.Column(db.Integer)
+    time = db.Column(db.String(80))
+
+    def __init__(self, phone, delay, number, time):
+        self.phone = phone
+        self.delay = delay
+        self.number = number
+        self.time = time
+
+    def __repr__(self):
+        return '<Phone>: ' + self.phone + ' <Delay>: ' + str(self.delay) + ' <Number>: ' + str(self.number) + ' <Time>: ' + self.time
