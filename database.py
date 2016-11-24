98620f34d33007a09eb61f49b8f2012ec6c023ce
RaphaelLu <ronilu66@hotmail.com>
2016-11-23 19:58:57 -0800

    fixed postgresql url

--- database.py
+++ database.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+import os 
 app = Flask(__name__)
+
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///calls.db'
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['HEROKU_POSTGRESQL_CYAN_URL']
 db = SQLAlchemy(app)
 
 
